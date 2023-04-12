680534b1fed922d0aa489fea25b75679f63ce3f1
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-12 05:03:29 +0300

    smth works

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('./app')
+// const app = require('./app');
+const express = require('express');
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send('<h2>helloo<h2>');
+});
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
-})
+});
