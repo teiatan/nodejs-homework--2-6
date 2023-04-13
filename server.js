ec1b204f1eef813c85d4bf34c4cd52b3eae47ad7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-13 22:17:42 +0300

    Revert "smth works"
    This reverts commit 680534b1fed922d0aa489fea25b75679f63ce3f1.

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-// const app = require('./app');
-const express = require('express');
-const app = express();
-
-
-app.get("/", (req, res) => {
-  res.send('<h2>helloo<h2>');
-});
+const app = require('./app')
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
-});
+})
