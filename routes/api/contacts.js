31771153085d17c6ee06a0f294e4841bb3f10134
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-13 22:42:16 +0300

    get contacts from json

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,32 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const { listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact } = require("../../models/contacts");
+
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const result = await listContacts();
+  res.json(result);
+});
 
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
-module.exports = router
+module.exports = router;
