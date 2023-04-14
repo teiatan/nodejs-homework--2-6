4cbadf5978d8812a7b492bddc98b8c500d329783
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-14 03:17:59 +0300

    delete extra info

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,18 @@
 const express = require('express');
-
 const { listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact } = require("../../controllers/contacts");
-
 const {contactsSchema} = require("../../shemas");
 const {validateBody} = require("../../middlewares");
 
 const router = express.Router();
 
 router.get('/', listContacts);
-
 router.get('/:contactId', getContactById);
-
 router.post('/', validateBody(contactsSchema), addContact);
-
 router.delete('/:contactId', removeContact);
-
 router.put('/:contactId', validateBody(contactsSchema), updateContact);
 
 module.exports = router;
