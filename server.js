b56db099c41f4941a510d88d4d2d8b044c3b47b7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-14 18:19:15 +0300

    write local environment

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const app = require('./app');
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT} = process.env;
 
 const mongoose = require("mongoose");
 
-
-// mongoose.set('strictQuery', true);
-
 mongoose.connect(DB_HOST)
-  .then(()=>app.listen(3000))
+  .then(()=>app.listen(PORT))
   .catch(error => {
     console.log(error.message);
     process.exit(1);
