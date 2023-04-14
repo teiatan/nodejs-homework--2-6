ab2ef53533a4fe45d248d83f7d6f09dfbcb6f9e8
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-15 01:12:16 +0300

    add favorite updating and finish other functions

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,49 +1,59 @@
 // const contacts = require("../models/contacts/contacts");
-const Contact = require("../models/contacts/contact")
+const {Contact} = require("../models/contact")
 
-const {/* HttpError, */ ControllerWrapper} = require("../utils/index");
+const {HttpError, ControllerWrapper} = require("../utils/index");
 
 const listContacts = async (req, res) => {
   const result = await Contact.find();
   res.json(result);    
 };
 
-// const getContactById = async (req, res,) => {
-//   const {contactId} = req.params;
-//   const result = await contacts.getContactById(contactId);
-//   if(!result) {
-//     throw HttpError(404, "Not found. Contact with such id doesn't exist");
-//   }
-//   res.json(result);
-// };
+const getContactById = async (req, res) => {
+  const {contactId} = req.params;
+  const result = await Contact.findById(contactId, "-createdAt -updatedAt");
+  if(!result) {
+    throw HttpError(404, "Not found. Contact with such id doesn't exist");
+  }
+  res.json(result);
+};
 
-// const removeContact = async (req, res) => {
-//   const {contactId} = req.params;
-//   const result = await contacts.removeContact(contactId);
-//   if(!result) {
-//     throw HttpError(404, "Not found. Contact with such id didn't exist");
-//   };
-//   res.json({"message": "contact deleted"});
-// };
+const removeContact = async (req, res) => {
+  const {contactId} = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if(!result) {
+    throw HttpError(404, "Not found. Contact with such id didn't exist");
+  };
+  res.json({"message": "contact deleted"});
+};
 
 const addContact = async (req, res) => {
   const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
-// const updateContact = async (req, res) => {
-//   const {contactId} = req.params;
-//   const result = await contacts.updateContact(contactId, req.body);
-//   if(!result) {
-//     throw HttpError(404, "Not found. Contact with such id doesn't exist");
-//   };
-//   res.json(result);
-// };
+const updateContact = async (req, res) => {
+  const {contactId} = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+  if(!result) {
+    throw HttpError(404, "Not found. Contact with such id doesn't exist");
+  };
+  res.json(result);
+};
+
+const updateFavorite = async (req, res) => {
+  const {contactId} = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+  if(!result) {
+    throw HttpError(404, "Not found. Contact with such id doesn't exist");
+  };
+  res.json(result);
+};
 
 module.exports = {
   listContacts: ControllerWrapper(listContacts),
-  // getContactById: ControllerWrapper(getContactById),
-  // removeContact: ControllerWrapper(removeContact),
+  getContactById: ControllerWrapper(getContactById),
+  removeContact: ControllerWrapper(removeContact),
   addContact: ControllerWrapper(addContact),
-  // updateContact: ControllerWrapper(updateContact)
+  updateContact: ControllerWrapper(updateContact),
+  updateFavorite: ControllerWrapper(updateFavorite)
 };
---
+++ models/contact.js
@@ -0,0 +1,64 @@
+const {Schema, model} = require("mongoose");
+const { handleMongooseError } = require("../utils");
+const Joi = require("joi");
+
+const relationships = ["famile", "work", "friend", "service"];
+const dateRegExp = /(0?[1-9]|[12][0-9]|3[01])[- _](January|February|March|Aprel|May|June|July|August|September|October|November|December)[- _]((19|20)\d\d)/;
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    }, 
+    dateOfBirth: {
+        type: String,
+        match: dateRegExp,
+    },
+    relationship: {
+        type: String,
+        enum: relationships,
+    }
+}, {
+    versionKey: false,
+    timestamps: true,
+});
+
+contactSchema.post("save", handleMongooseError);
+
+const contactsBasicSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+    favorite: Joi.boolean(),
+    dateOfBirth: Joi.string().pattern(dateRegExp),
+    relationship: Joi.string().valid(...relationships),
+
+});
+
+const favoriteschema = Joi.object({
+    favorite: Joi.boolean(),
+})
+
+const schemas = {
+    contactsBasicSchema,
+    favoriteschema
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+    Contact,
+    schemas
+};
