69681cc2ba8437d9d00ff2c32d409fbf40d208e9
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-30 00:21:09 +0300

    add owner to contacts

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -15,6 +15,7 @@ const authenticate = async (req, res, next) => {
         if(!user) {
             next(HttpError(401, "Wrong token"));
         };
+        req.user = user;
         next();
     } catch(error) {
         next(HttpError(401));
