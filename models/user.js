efba808cf4d8d9cb511b3a0952566fb1e5838011
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-29 20:25:34 +0300

    add email regex and user name

--- models/user.js
+++ models/user.js
@@ -2,14 +2,22 @@ const {Schema, model} = require("mongoose");
 const { handleMongooseError } = require("../utils");
 const Joi = require("joi");
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     password: {
         type: String,
+        minlength: 8,
         required: [true, 'Password is required'],
     },
     email: {
         type: String,
         required: [true, 'Email is required'],
+        match: emailRegex,
         unique: true,
     },
     subscription: {
