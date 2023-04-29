bce68a330e8d0abe5108053d4bb6f1ff29f682b7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-29 21:30:57 +0300

    add hash password

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,16 +1,19 @@
-const {User} = require("../../models/user")
+const {User} = require("../../models/user");
+const bcrypt = require("bcrypt");
 
 const {ControllerWrapper, HttpError} = require("../../utils/index");
 
 const register = async (req, res) => {
-    const {email} = req.body;
+    const {email, password} = req.body;
     const user = await User.findOne({email});
 
     if(user){
         throw HttpError(409, `User with email ${email} already exists`);
     };
 
-    const newUser = await User.create(req.body);
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await User.create({...req.body, password: hashPassword});
     res.status(201).json(newUser);
 };
 
