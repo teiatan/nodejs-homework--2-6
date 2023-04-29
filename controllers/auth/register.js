ef9462e451755b9a5aa414d990e5a42aabf1893e
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-29 21:17:57 +0300

    make email unique

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,8 +1,15 @@
 const {User} = require("../../models/user")
 
-const {ControllerWrapper} = require("../../utils/index");
+const {ControllerWrapper, HttpError} = require("../../utils/index");
 
 const register = async (req, res) => {
+    const {email} = req.body;
+    const user = await User.findOne({email});
+
+    if(user){
+        throw HttpError(409, `User with email ${email} already exists`);
+    };
+
     const newUser = await User.create(req.body);
     res.status(201).json(newUser);
 };
