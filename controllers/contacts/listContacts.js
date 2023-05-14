567eaceb6801b54088c94b96bd749e598baa9d98
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-15 01:10:10 +0300

    get only favorite contacts

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,9 +4,10 @@ const {ControllerWrapper} = require("../../utils/index");
 
 const listContacts = async (req, res) => {
   const {_id:owner} = req.user;
-  const {page = 1, limit = 20} = req.query;
-  const skip = (page-1*limit);
-  const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "name email subscription");
+  const {page = 1, limit = 20, favorite} = req.query;
+  const skip = (page-1)*limit;
+  const filter = favorite ? { owner, favorite } : { owner };
+  const result = await Contact.find(filter, "-createdAt -updatedAt", {skip, limit, favorite}).populate("owner", "name email subscription");
   res.json(result);    
 };
 
