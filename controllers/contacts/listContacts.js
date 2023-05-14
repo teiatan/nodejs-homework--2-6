983f17918a59fd9ab2560ba9042dc43d3c6251e7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-14 10:02:13 +0300

    add owner to listContacts

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,7 +3,8 @@ const {Contact} = require("../../models/contact")
 const {ControllerWrapper} = require("../../utils/index");
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const {_id:owner} = req.user;
+  const result = await Contact.find({owner}).populate("owner");
   res.json(result);    
 };
 
