2601fd7494ae519f84608b5b3bca7c107a3b07eb
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-15 01:19:44 +0300

    fix logout

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {validateBody} = require("../middlewares");
+const {validateBody, authenticate} = require("../middlewares");
 const {schemas} = require("../models/user");
 const {register, login, logout, getUsersData} = require("../controllers/users");
 
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/register', validateBody(schemas.registerSchema), register);
 router.post('/login', validateBody(schemas.loginSchema), login);
-router.post('/logout', validateBody(schemas.loginSchema), logout);
+router.post('/logout', authenticate, logout);
 router.get('/current', validateBody(schemas.loginSchema), getUsersData);
 
 module.exports = router;
