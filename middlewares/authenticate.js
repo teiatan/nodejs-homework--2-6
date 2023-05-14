62533e31156b274fcf21c066a465603eaa9ec697
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-15 01:47:56 +0300

    fix logout

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -12,7 +12,7 @@ const authenticate = async (req, res, next) => {
     try {
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        if(!user || !user.token || !user.token !== token) {
+        if(!user || !user.token || user.token !== token) {
             next(HttpError(401, "Wrong token"));
         };
         req.user = user;
