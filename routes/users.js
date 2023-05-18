4e79737d63a93fda74deb44f5fa43dea5aa2cab8
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-18 07:49:26 +0300

    add upload

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const {validateBody, authenticate} = require("../middlewares");
+const {validateBody, authenticate, upload} = require("../middlewares");
 const {schemas} = require("../models/user");
 const {register, login, logout, getUsersData, updateSubscription} = require("../controllers/users");
 
 const router = express.Router();
 
-router.post('/register', validateBody(schemas.registerSchema), register);
+router.post('/register', validateBody(schemas.registerSchema), upload.single("avatarURL"), register);
 router.post('/login', validateBody(schemas.loginSchema), login);
 router.post('/logout', authenticate, logout);
 router.get('/current', authenticate, getUsersData);
 router.patch('/', authenticate, validateBody(schemas.subscriptionSchema), updateSubscription);
+router.patch('/avatars')
 
 module.exports = router;
